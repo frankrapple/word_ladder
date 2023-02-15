e1eb01adbf6b0a91527c92471e983a13db02d5a1
Frankrapple <frankrapplebaum@gmail.com>
2023-02-14 22:16:05 -0800

    word ladder

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,9 @@
 #!/bin/python3
 
+from collections import deque
+
+import copy
+
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
@@ -28,7 +32,29 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    if start_word == end_word:
+        return [start_word]
 
+    stack = [start_word]
+    queue = [stack]
+    dictionary_file = open(dictionary_file).read().split()
+
+    while queue:
+        curr_stack = queue.pop(0)
+        curr_word = curr_stack[-1]
+
+        for word in dictionary_file:
+            if _adjacent(curr_word, word):
+                if word == end_word:
+                    curr_stack.append(word)
+                    return curr_stack
+                else:
+                    copy_stack = curr_stack.copy()
+                    copy_stack.append(word)
+                    queue.append(copy_stack)
+                    dictionary_file.remove(word)
+
+    return None
 
 def verify_word_ladder(ladder):
     '''
@@ -40,7 +66,24 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    if ladder == None:
+        return False
 
+    if len(ladder) == 0:
+        return False
+	
+    count = 0
+    
+    for i in range(len(ladder) - 1):
+        if ladder[i] == ladder[i+1]:
+            return False
+        B = _adjacent(ladder[i], ladder[i+1])
+        if B == True:
+            count += 1
+    if count == (len(ladder) - 1):
+        return True
+    else:
+        return False
 
 def _adjacent(word1, word2):
     '''
@@ -52,3 +95,19 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+
+    if word1 == word2:
+        return False
+    if len(word1) != len(word2):
+        return False
+    count = 0
+    index = 0
+    while index<len(word1) and index<len(word1):
+        for i in word1:
+            if word1[index] != word2[index]:
+                count += 1
+            index +=1
+        if count == 1:
+            return(True)
+        else:
+            return(False)
